bd3673a4695a9d803e038c5fd39bc9c14f9f68a1
rockyrock39 <rishabhgautam0433@gmail.com>
2024-02-13 21:54:53 +0530

    checkout page adder

--- src/test/java/stepDefinations/Hooks.java
+++ src/test/java/stepDefinations/Hooks.java
@@ -1,9 +1,13 @@
 package stepDefinations;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import Selenuium.TestCaseBasePage;
 import base.TestBase;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hooks {
 	
@@ -51,8 +55,16 @@ public class Hooks {
 	    }
 	    
 	    @After
-	    public void TearDown()
+	    public void TearDown(Scenario scenerio)
+	    
 	    {
+	    	if (scenerio.isFailed())
+	    	{
+	    		TakesScreenshot screen = (TakesScreenshot)TestBase.getDriver();
+	    		byte [] image = screen.getScreenshotAs(OutputType.BYTES);
+	    		scenerio.attach(image, "image/png", "screenImage");
+	    		
+	    	}
 	    	
 	    }
 
