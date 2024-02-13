03a811fdfd5b22f8dc94bf0f9b6c52e49aa34d7a
rockyrock39 <rishabhgautam0433@gmail.com>
2024-02-13 07:33:46 +0000

    Update ShoppingCartPage.java
    commit

--- src/test/java/page/ShoppingCartPage.java
+++ src/test/java/page/ShoppingCartPage.java
@@ -1,22 +1,39 @@
 package page;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 public class ShoppingCartPage {
 
-	@FindBy(how = How.CSS, using = "ul li.preview")
-	WebElement Textbox1;
-	@FindBy(how = How.XPATH, using = "//button[contains(text(),'Add to cart')]")
+	@FindAll(value= @FindBy(css  =  "ul li"))
+	List<WebElement> Textbox1;
+	@FindBy(how = How.XPATH, using = "//button[contains(text(),'Checkout']")
 	WebElement addtobutton;
 
 	public ShoppingCartPage(WebDriver driver) {
 		// TODO Auto-generated constructor stub
 		PageFactory.initElements(driver, this);
 
+	}
+	public  boolean isitemSelected()
+	{
+		if(Textbox1.size()>0)
+		{
+			return true;
+		}else
+			return false;
+		
+	}
+	
+	public void clickcheckout()
+	{
+		addtobutton.click();
 	}
 
 }
